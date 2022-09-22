9a52f631abf873c7889a13dcfe2345070c1490d2
YuichiKamishiro <yuichikamishiro@gmail.com>
2022-09-22 21:57:15 +0300

    apples

--- main.cpp
+++ main.cpp
@@ -5,29 +5,39 @@
 
 int main(){
     initscr();
-    
-    int y, x;
-    getmaxyx(stdscr, y, x);
-    snake sn({y / 2, x / 2});
     noecho();
     cbreak();
+    
+    int y, x, winy, winx;
+    getmaxyx(stdscr, y, x);
+    
+    snake sn({5, 5});
+    
+    WINDOW *win = newwin(y / 2 + 5, x / 2, y / 4, x / 4);
+    getmaxyx(win, winy, winx);
 
     std::chrono::steady_clock::time_point st = std::chrono::steady_clock::now();
+
+    curs_set(0);
 
     while(true){
         std::chrono::steady_clock::time_point en = std::chrono::steady_clock::now();
         if(std::chrono::duration_cast<std::chrono::milliseconds>(en - st).count() >= 100){
-            clear();
+            wclear(win);
             st = std::chrono::steady_clock::now();
         }
 
+
+        sn.collision(1, winy - 1, 1, winx - 1);
         sn.control();
+        sn.spawn(1, winy - 2, 1, winx - 2);
         sn.move();
-        sn.draw(stdscr);
-        
-        box(stdscr, 0, 0);
+
+        box(win, 0, 0);
+        sn.draw(win);
     
         refresh();
+        wrefresh(win);
     }
 
     endwin();
--- snake.hpp
+++ snake.hpp
@@ -1,61 +1,107 @@
+#include <ctime>
 #include <curses.h>
 #include <chrono>
 #include <functional>
 #include <vector>
 
-class node{
+class point {
 public:
-    int past_x, past_y, current_x, current_y;
-    node(std::pair<int, int>); 
+    int y, x;
+    point(int y, int x) : y(y), x(x) {}
+    point() : y(0), x(0) {}
 };
 
-class snake{
+class node {
+public:
+    point past_pos, current_pos;
+    node(point current_pos) : current_pos(current_pos) {} 
+};
+
+class snake {
 public:
     std::chrono::steady_clock::time_point start;
+    std::chrono::steady_clock::time_point spawn_timer;
+    
     std::vector<node> snake_arr;
-    int dir = 'w';
+    bool eaten = true;
+    point apple_pos;
+    int dir = '0';
 
     snake(std::pair<int, int>);
+
     void move();
+    void spawn(int, int, int, int);
     void control();
-    void collision(); 
+    void collision(int, int, int, int); 
     void draw(WINDOW *);
 };
 
-void snake::move(){
+void snake::move() {
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    if(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() >= 150){
+
+    if(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() >= 200){
         start = std::chrono::steady_clock::now();
-        for(int i = 0; i < snake_arr.size(); ++i){
-        switch(dir){
-            case 'w': snake_arr[i].past_y = snake_arr[i].current_y; snake_arr[i].current_y -=1;break;
-            case 's': snake_arr[i].past_y = snake_arr[i].current_y; snake_arr[i].current_y +=1;break;
-            case 'a': snake_arr[i].past_x = snake_arr[i].current_x; snake_arr[i].current_x -=1;break;
-            case 'd': snake_arr[i].past_x = snake_arr[i].current_x; snake_arr[i].current_x +=1;break;
-        } 
-    }}
+
+        snake_arr[0].past_pos.x = snake_arr[0].current_pos.x;
+        snake_arr[0].past_pos.y = snake_arr[0].current_pos.y;
+        
+        if(dir == 'w') { snake_arr[0].current_pos.y -= 1; }
+        if(dir == 's') { snake_arr[0].current_pos.y += 1; }
+        if(dir == 'a') { snake_arr[0].current_pos.x -= 1; }
+        if(dir == 'd') { snake_arr[0].current_pos.x += 1; }
+
+        for(int i = 1; i < snake_arr.size(); ++i){
+            snake_arr[i].past_pos.x = snake_arr[i].current_pos.x;
+            snake_arr[i].past_pos.y = snake_arr[i].current_pos.y;
+
+            snake_arr[i].current_pos.x = snake_arr[i - 1].past_pos.x;
+            snake_arr[i].current_pos.y = snake_arr[i - 1].past_pos.y;
+        }
+    }
 }
 
-void snake::control(){
-    timeout(1);
+void snake::control() {
+    timeout(0);
+    
     char c = getch();
-    switch(c){
-        case 'w': dir = 'w';break;
-        case 'a': dir = 'a';break;
-        case 's': dir = 's';break;
-        case 'd': dir = 'd';break;
+    
+    switch(c) {
+        case 'w': dir = 'w'; break;
+        case 'a': dir = 'a'; break;
+        case 's': dir = 's'; break;
+        case 'd': dir = 'd'; break;
     }
 }
 
-void snake::draw(WINDOW *win){
-    for(int i = 0; i < snake_arr.size(); ++i){ 
-        mvwprintw(win, snake_arr[i].current_y, snake_arr[i].current_x, "#");
-    }
+void snake::draw(WINDOW *win){ 
+    for(int i = 0; i < snake_arr.size(); ++i) { mvwaddch(win, snake_arr[i].current_pos.y, snake_arr[i].current_pos.x, '+'); }
+    mvwaddch(win, apple_pos.y, apple_pos.x, '*');
 }
 
-node::node(std::pair<int, int> p) : current_y(p.first), current_x(p.second){}
-
-snake::snake(std::pair<int, int> p){
+snake::snake(std::pair<int, int> p) {
     snake_arr.push_back(node({p.first, p.second}));
     start = std::chrono::steady_clock::now();
+    spawn_timer = std::chrono::steady_clock::now();
+}
+
+
+void snake::spawn(int miny, int maxy, int minx, int maxx) {
+    if(eaten) {
+        point rpoint(rand() % maxy + miny, rand() % maxx + minx);
+        apple_pos = rpoint;
+        eaten = false;
+    } else if(!eaten){
+        if(snake_arr[0].current_pos.y == apple_pos.y
+        && snake_arr[0].current_pos.x == apple_pos.x){ 
+            snake_arr.push_back(node(point(snake_arr[snake_arr.size() - 1].past_pos.y,
+            snake_arr[snake_arr.size() - 1].past_pos.x)));  
+            eaten = true;
+            apple_pos.x = 0; apple_pos.y = 0;
+        }
+    }
+}
+
+void snake::collision(int miny, int maxy, int minx, int maxx) {
+    if(snake_arr[0].current_pos.x >= maxx || snake_arr[0].current_pos.x <= minx) exit(3);
+    if(snake_arr[0].current_pos.y >= maxy || snake_arr[0].current_pos.y <= miny) exit(3);
 }
